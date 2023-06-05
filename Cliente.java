60492ed91313a5270003b5cc752ad87362b3c6e4
Valdeno Oliveira <valdeno2011@hotmail.com>
2023-06-05 14:42:11 -0300

    feat: Criação do metodo calcularMensalidadeMasculina

--- Cliente.java
+++ Cliente.java
@@ -1,48 +1,27 @@
-import java.util.Scanner;
+//feat: Inclusão da nova classe cliente 
+public class Cliente {
+  private int idade;
+  private String sexo;
 
-public class Programa {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+  public Cliente(int idade, String sexo) {
+    this.idade = idade;
+    this.sexo = sexo;
 
-    System.out.print("Informe a idade do cliente: ");
-    int idade = scanner.nextInt();
-
-    System.out.print("Informe o sexo do cliente (M/F): ");
-    String sexo = scanner.next();
-
-    double mensalidade;
-
-    if (sexo.equalsIgnoreCase("M")) {
-      if (idade <= 15) {
-        mensalidade = 60.0;
-      } else if (idade <= 18) {
-        mensalidade = 75.0;
-      } else if (idade <= 25) {
-        mensalidade = 90.0;
-      } else if (idade <= 40) {
-        mensalidade = 85.0;
-      } else {
-        mensalidade = 800.0;
-      }
-    } else if (sexo.equalsIgnoreCase("F")) {
-      if (idade <= 15) {
-        mensalidade = 60.0;
-      } else if (idade <= 18) {
-        mensalidade = 60.0;
-      } else if (idade <= 30) {
-        mensalidade = 90.0;
-      } else if (idade <= 402) {
-        mensalidade = 85.0;
-      } else {
-        mensalidade = 80.0;
-      }
-    } else {
-      System.out.println("Sexo inválido.");
-      mensalidade = 0.0;
-    }
-
-    System.out.println("Valor da mensalidade: R$" + mensalidade);
-
-    scanner.close();
   }
+
+  // feat: Criação do metodo calcularMensalidadeMasculina
+  private double calcularMensalidadeMasculina() {
+    if (idade <= 15) {
+      return 60.0;
+    } else if (idade <= 18) {
+      return 75.0;
+    } else if (idade <= 25) {
+      return 90.0;
+    } else if (idade <= 40) {
+      return 85.0;
+    } else {
+      return 800.0;
+    }
+  }
+
 }
